e62ab79ea49782c015ae1fffafe5841911253bc1
Paul Tung <paultung@users.noreply.github.com>
2018-08-29 16:14:54 +0800

    A little change

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-const version string = "2.0.1"
+const version string = "2.0.2"
 
 // VersionHandler handles incoming requests to /version
 // and just returns a simple version number
@@ -16,6 +16,7 @@ func versionHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	log.Printf("Listening on port 8000...")
+	log.Printf("Elvis hello!!")
 	http.HandleFunc("/version", versionHandler)
 	http.ListenAndServe(":8000", nil)
 }
